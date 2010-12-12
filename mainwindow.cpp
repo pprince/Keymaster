4e3dd3f64d614c357695fa53ff5e6fc9b34de4a9
U-thinkseven\pprince <pprince@thinkseven.(none)>
2010-12-12 15:45:11 -0500

    First alpha complete, see Output/setup.exe

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -24,30 +24,6 @@ MainWindow::MainWindow(QWidget *parent) :
     combined_item->setPos(0,-1*100*key);
     combined_scene->addItem(combined_item);
     ui->combined_gv->setScene(combined_scene);
-
-    signatures_scene = new QGraphicsScene (0, 0, 100, 100);
-    signatures_item  = new QGraphicsSvgItem (":/keymaster/artwork/ALL_KEYS.svg");
-    signatures_item->setPos(0,-1*100*key);
-    signatures_scene->addItem(signatures_item);
-    ui->signatures_gv->setScene(signatures_scene);
-
-    majortext_scene = new QGraphicsScene (0, 0, 100, 100);
-    majortext_item  = new QGraphicsSvgItem (":/keymaster/artwork/ALL_KEYS.svg");
-    majortext_item->setPos(-100,-1*100*key);
-    majortext_scene->addItem(majortext_item);
-    ui->majortext_gv->setScene(majortext_scene);
-
-    minortext_scene = new QGraphicsScene (0, 0, 100, 100);
-    minortext_item  = new QGraphicsSvgItem (":/keymaster/artwork/ALL_KEYS.svg");
-    minortext_item->setPos(-200,-1*100*key);
-    minortext_scene->addItem(minortext_item);
-    ui->minortext_gv->setScene(minortext_scene);
-
-    numbers_scene = new QGraphicsScene (0, 0, 100, 100);
-    numbers_item  = new QGraphicsSvgItem (":/keymaster/artwork/ALL_KEYS.svg");
-    numbers_item->setPos(-300,-1*100*key);
-    numbers_scene->addItem(numbers_item);
-    ui->numbers_gv->setScene(numbers_scene);
 }
 
 MainWindow::~MainWindow()
@@ -75,10 +51,6 @@ void MainWindow::on_next_key_button_clicked()
     int key = current_key;
 
     combined_item->setPos(0,-1*100*key);
-    signatures_item->setPos(0,-1*100*key);
-    majortext_item->setPos(-100,-1*100*key);
-    minortext_item->setPos(-200,-1*100*key);
-    numbers_item->setPos(-300,-1*100*key);
 }
 
 void MainWindow::on_exit_button_clicked()
