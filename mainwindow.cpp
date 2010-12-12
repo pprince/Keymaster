f67c36f76700d7b1ce5d2b46ea118ea6bbc768d5
Paul Prince <paul@littlebluetech.com>
2010-12-12 04:46:34 -0500

    late-night edits.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -18,31 +18,31 @@ MainWindow::MainWindow(QWidget *parent) :
 
     QGraphicsScene   *combined_scene = new QGraphicsScene (0, 0, 400, 100);
     combined_item  = new QGraphicsSvgItem (":/keymaster/artwork/ALL_KEYS.svg");
-    combined_item->setPos(0,-1400);
+    combined_item->setPos(0,0);
     combined_scene->addItem(combined_item);
     ui->combined_gv->setScene(combined_scene);
 
     QGraphicsScene   *signatures_scene = new QGraphicsScene (0, 0, 100, 100);
     QGraphicsSvgItem *signatures_item  = new QGraphicsSvgItem (":/keymaster/artwork/ALL_KEYS.svg");
-    signatures_item->setPos(0,-1400);
+    signatures_item->setPos(0,0);
     signatures_scene->addItem(signatures_item);
     ui->signatures_gv->setScene(signatures_scene);
 
     QGraphicsScene   *majortext_scene = new QGraphicsScene (0, 0, 100, 100);
     QGraphicsSvgItem *majortext_item  = new QGraphicsSvgItem (":/keymaster/artwork/ALL_KEYS.svg");
-    majortext_item->setPos(-100,-1400);
+    majortext_item->setPos(-100,0);
     majortext_scene->addItem(majortext_item);
     ui->majortext_gv->setScene(majortext_scene);
 
     QGraphicsScene   *minortext_scene = new QGraphicsScene (0, 0, 100, 100);
     QGraphicsSvgItem *minortext_item  = new QGraphicsSvgItem (":/keymaster/artwork/ALL_KEYS.svg");
-    minortext_item->setPos(-200,-1400);
+    minortext_item->setPos(-200,0);
     minortext_scene->addItem(minortext_item);
     ui->minortext_gv->setScene(minortext_scene);
 
     QGraphicsScene   *numbers_scene = new QGraphicsScene (0, 0, 100, 100);
     QGraphicsSvgItem *numbers_item  = new QGraphicsSvgItem (":/keymaster/artwork/ALL_KEYS.svg");
-    numbers_item->setPos(-300,-1400);
+    numbers_item->setPos(-300,0);
     numbers_scene->addItem(numbers_item);
     ui->numbers_gv->setScene(numbers_scene);
 }
